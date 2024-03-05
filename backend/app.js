b6b60d5457ecfd6c8d447b5e2e89ec969023ca3c
Konalawebsites <atfitcad@gmail.com>
2024-03-05 17:48:55 +0200

    aws, recipe photos

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,11 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const recipesRouter = require('./controllers/recipes')
-<<<<<<< HEAD
 const { awsRouter } = require('./controllers/multer')
-=======
->>>>>>> e0f7442b6eadb312a158acca03127aa21e347e42
 const middleware = require('./utils/middleware.js')
 const logger = require('./utils/logger.js')
 const mongoose = require('mongoose')
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e0f7442b6eadb312a158acca03127aa21e347e42
 mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -35,10 +28,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/recipes', recipesRouter)
-<<<<<<< HEAD
 app.use('/api/recipeImages', awsRouter)
-=======
->>>>>>> e0f7442b6eadb312a158acca03127aa21e347e42
 
 // if (process.env.NODE_ENV === 'test') {
 //   const testingRouter = require('./controllers/testing')
